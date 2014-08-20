7714e8c7f84eeb5181f7dbab230f9dee1388ab40
Josie Wright <student@epicodus.com>
2014-08-20 15:37:23 -0700

    adds contributions

--- UI.rb
+++ UI.rb
@@ -14,17 +14,22 @@ def menu
   loop do
     puts "PRESS (a)uthors, (t)itles, (c)ontributions OR e(x)it"
     main_choice = gets.chomp
+
     if main_choice == 'a'
       puts "PRESS 'aa' TO ADD AN AUTHOR"
       puts "PRESS 'la' TO LIST AUTHORS"
+      puts "PRESS 'at' TO ADD AN AUTHOR TO A TITLE"
       a_choice = gets.chomp
       if a_choice == 'aa'
         add_author
       elsif a_choice == 'la'
         list_authors
+      elsif a_choice == 'at'
+        add_author_to_title
       else
         puts "ENTER A VALID KEY"
       end
+
     elsif main_choice == 't'
       puts "PRESS 'at' TO ADD A TITLE"
       puts "PRESS 'lt' TO LIST TITLES"
@@ -36,6 +41,7 @@ def menu
       else
         puts "ENTER A VALID KEY"
       end
+
     elsif main_choice == 'x'
       exit
     end
@@ -53,8 +59,19 @@ end
 def list_authors
   puts '********** AUTHORS **********'
   Author.all.each do |author|
-    puts author.name
+    puts "#{author.id}: #{author.name}"
   end
+end
+
+def add_author_to_title
+  list_authors
+  puts "ENTER AN AUTHOR ID"
+  author_input = gets.chomp
+  list_titles
+  puts "ENTER TITLE ID"
+  title_input = gets.chomp
+  DB.exec("INSERT INTO contributions (author_id, title_id) VALUES (#{author_input}, #{title_input})")
+  puts "CONTRIBUTION ADDED"
 end
 
 def add_title
@@ -68,7 +85,7 @@ end
 def list_titles
   puts '********** TITLES **********'
   Title.all.each do |title|
-    puts title.name
+    puts "#{title.id}: #{title.name}"
   end
 end
 
