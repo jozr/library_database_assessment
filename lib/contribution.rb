f40403da9c902a788c7174b700ce1d6369dd53e2
Josie Wright <student@epicodus.com>
2014-08-20 11:42:12 -0700

    allows for deleting of specific contributions

---
+++ lib/contribution.rb
@@ -0,0 +1,39 @@
+class Contribution
+
+  attr_reader :title_id, :author_id, :id
+
+  def initialize(hash)
+    @title_id = hash['title_id']
+    @author_id = hash['author_id']
+    @id = hash['id']
+  end
+
+  def save
+    results = DB.exec("INSERT INTO contributions (author_id, title_id) VALUES (#{@author_id}, #{@title_id}) RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def self.all
+    results = DB.exec('SELECT * FROM contributions;')
+    contributions = []
+    results.each do |result|
+      author_id = result['author_id'].to_i
+      title_id = result['title_id'].to_i
+      id = result['id'].to_i
+      contributions << Contribution.new({'author_id' => author_id, 'title_id' => title_id, 'id' => id})
+    end
+  contributions
+  end
+
+  def ==(another_name)
+    self.author_id == another_name.author_id && self.title_id == another_name.title_id
+  end
+
+  def remove(author_name_input, title_name_input)
+    a_result = DB.exec("SELECT * FROM authors WHERE name = '#{author_name_input}'")
+    a_id = a_result.first['id'].to_i
+    t_result = DB.exec("SELECT * FROM titles WHERE name = '#{title_name_input}'")
+    t_id = a_result.first['id'].to_i
+    DB.exec("DELETE FROM contributions WHERE title_id = #{t_id} AND author_id = #{a_id}")
+  end
+end
---
+++ spec/contribution_spec.rb
@@ -0,0 +1,39 @@
+require 'rspec_helper'
+
+describe Contribution do
+
+  it 'is initialized with a name' do
+    contribution = Contribution.new({'author_id' => 1, 'title_id' => 3})
+    contribution.should be_instance_of Contribution
+  end
+
+  it 'lets you save multiple contributions to the database' do
+    contribution = Contribution.new({'author_id' => 2, 'title_id' => 5})
+    contribution.save
+    contribution_two = Contribution.new({'author_id' => 2, 'title_id' => 9})
+    contribution_two.save
+    Contribution.all.should eq [contribution, contribution_two]
+  end
+
+  it 'treats contributions as equal if author id and title id match' do
+    contribution = Contribution.new({'author_id' => 3, 'title_id' => 4})
+    contribution_two = Contribution.new({'author_id' => 3, 'title_id' => 4})
+    contribution.should eq contribution_two
+  end
+
+  it 'deletes specific contributions by author and title' do
+    title = Title.new({'name' => 'Cookbook'})
+    title.save
+    author = Author.new({'name' => 'Jane'})
+    author.save
+    contribution = Contribution.new({'author_id' => author.id, 'title_id' => title.id})
+    title_two = Title.new({'name' => 'Favorite Tea'})
+    title_two.save
+    author_two = Author.new({'name' => 'Earl Grey'})
+    author_two.save
+    contribution_two = Contribution.new({'author_id' => author_two.id, 'title_id' => title_two.id})
+    contribution_two.save
+    contribution.remove('Jane', 'Cookbook')
+    Contribution.all.should eq [contribution_two]
+  end
+end
